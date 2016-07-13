12dfe9fd483e097bb7a60b4949fe012a00d96f2e
Ankit Ranjan <me@ankit.io>
2016-07-12 18:20:55 -0700

    Added basic infra to hello world to resolve #5

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+// Copilot Volunteer Interface
+
 var express = require('express');
 var app = express();
+var dotenv = require('dotenv');
+dotenv.load();
 
 app.get('/', function (req, res) {
       res.send('Hello World!');
 });
 
-app.listen(3000);
+app.listen(process.env.port, process.env.HOSTNAME, function () {
+    console.log('Copilot volunteer interface successfully running at '
+        + process.env.HOSTNAME + ':' + process.env.PORT);
+});
 
