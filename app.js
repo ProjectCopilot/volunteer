cfe1d1ef31dfc11d8cd39570d5dca0c7e89975b7
gmittal <gautam@mittal.net>
2016-07-21 13:57:32 -0700

    setting up firebase and interface

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var app = express();
 var dotenv = require('dotenv').config({path: __dirname+'/.env'});
+var fs = require('fs');
 
 app.get('/', function (req, res) {
     res.redirect("/login");
@@ -14,6 +15,15 @@ app.get("/login", function (req, res) {
 
 app.get("/volunteer/:volunteerId", function (req, res) {
     res.sendFile(__dirname+'/static/index.html');
+});
+
+app.get("/volunteer/js/main.js", function (req, res) {
+    res.header("Content-Type", "application/javascript");
+    fs.readFile(__dirname + "/static/js/main.js", "utf-8", function (e, data) {
+      if (e) throw e;
+      var script = data.replace(/{HOSTNAME}/g, process.env.HOSTNAME).replace(/{PORT}/g, process.env.PORT).replace(/{FIREBASE_ID}/g, process.env.FIREBASE_ID).replace(/{FIREBASE_API_KEY}/g, process.env.FIREBASE_CLIENT_API_KEY);
+      res.send(script);
+    });
 });
 
 app.use("/volunteer", express.static(__dirname+"/static"));
--- configure.sh
+++ configure.sh
@@ -8,9 +8,14 @@ echo "What hostname do you want to run the app on?"
 read HOST
 echo "What port do you want to run the listener on?"
 read PORT
+echo "What is the Firebase Client API Key?"
+read FB_KEY
+echo "What is the Firebase database ID?"
+read FB_ID
 
 cat <<EOF > $CONFIG
 HOSTNAME=$HOST
 PORT=$PORT
+FIREBASE_CLIENT_API_KEY=$FB_KEY
+FIREBASE_ID=$FB_ID
 EOF
-
