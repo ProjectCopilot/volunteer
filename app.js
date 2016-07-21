613c62deff5dee61b4203835f9968ee655096b67
gmittal <gautam@mittal.net>
2016-07-21 15:46:18 -0700

    Pushing what is there so far.

--- app.js
+++ app.js
@@ -14,16 +14,12 @@ app.get("/login", function (req, res) {
 });
 
 app.get("/volunteer/:volunteerId", function (req, res) {
-    res.sendFile(__dirname+'/static/index.html');
-});
-
-app.get("/volunteer/js/main.js", function (req, res) {
-    res.header("Content-Type", "application/javascript");
-    fs.readFile(__dirname + "/static/js/main.js", "utf-8", function (e, data) {
-      if (e) throw e;
-      var script = data.replace(/{HOSTNAME}/g, process.env.HOSTNAME).replace(/{PORT}/g, process.env.PORT).replace(/{FIREBASE_ID}/g, process.env.FIREBASE_ID).replace(/{FIREBASE_API_KEY}/g, process.env.FIREBASE_CLIENT_API_KEY);
-      res.send(script);
-    });
+  res.header("Content-Type", "text/html");
+  fs.readFile(__dirname + "/static/index.html", "utf-8", function (e, data) {
+    if (e) throw e;
+    var script = data.replace(/{HOSTNAME}/g, process.env.HOSTNAME).replace(/{PORT}/g, process.env.PORT).replace(/{FIREBASE_ID}/g, process.env.FIREBASE_ID).replace(/{FIREBASE_API_KEY}/g, process.env.FIREBASE_CLIENT_API_KEY);
+    res.send(script);
+  });
 });
 
 app.use("/volunteer", express.static(__dirname+"/static"));
--- static/js/main.js
+++ static/js/main.js
@@ -3,20 +3,9 @@
 // Project Copilot Volunteer Client
 // Copyright 2016 Project Copilot
 
-(function () {
-  var HOSTNAME = "{HOSTNAME}";
-  var PORT = "{PORT}";
-  var FIREBASE_ID = "{FIREBASE_ID}";
-  var FIREBASE_API_KEY = "{FIREBASE_API_KEY}";
+// (function () {
 
-  var config = {
-    apiKey: FIREBASE_API_KEY,
-    authDomain: FIREBASE_ID+".firebaseapp.com",
-    databaseURL: "https://"+FIREBASE_ID+".firebaseio.com",
-    storageBucket: FIREBASE_ID+".appspot.com",
-  };
-  firebase.initializeApp(config);
 
-  var currentCaseID = "";
+  var db = firebase.database().ref();
 
-});
+// });
