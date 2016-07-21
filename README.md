9c651870d66c5c88d6e32b7318ee8fbd218b87c2
gmittal <gautam@mittal.net>
2016-07-21 12:48:53 -0700

    update README

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # volunteer
 Volunteer services.
+
+### Setup
+Populate ```.env``` file in project root with the following information:
+```
+PORT=XXXX
+HOSTNAME=[0.0.0.0|127.0.0.X|localhost|etc.]
+```
+
+Install the npm dependencies.
+```
+$ [sudo] npm install
+```
+
+Run the server.
+```
+$ node app.js
+```
--- app.js
+++ app.js
@@ -5,11 +5,14 @@ var app = express();
 var dotenv = require('dotenv').config({path: __dirname+'/.env'});
 
 app.get('/', function (req, res) {
-      res.send('Hello World!');
+    res.redirect("/login");
+});
+
+app.get("/login", function (req, res) {
+    res.send("WIP");
 });
 
 app.listen(process.env.port, process.env.HOSTNAME, function () {
     console.log('Copilot volunteer interface successfully running at '
         + process.env.HOSTNAME + ':' + process.env.PORT);
 });
-
