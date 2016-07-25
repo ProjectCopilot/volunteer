1c46ecde741ff807f41b51ad89f8ccd1445f8141
gmittal <gautam@mittal.net>
2016-07-25 13:17:33 -0700

    FIREBASE WORKS

--- static/js/main.js
+++ static/js/main.js
@@ -3,9 +3,24 @@
 // Project Copilot Volunteer Client
 // Copyright 2016 Project Copilot
 
-// (function () {
+(function () {
 
+  var provider = new firebase.auth.GoogleAuthProvider();
 
-  var db = firebase.database().ref();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    var email = error.email;
+    var credential = error.credential;
+    // ...
+  });
 
-// });
+  var db = firebase.database().ref("/");
+
+});
