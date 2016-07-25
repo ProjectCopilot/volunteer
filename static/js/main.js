d9a5601d7b52c564973b91d616256bcbc16bcd4f
gmittal <gautam@mittal.net>
2016-07-25 13:44:42 -0700

    Working on adding Firebase listeners

--- static/js/main.js
+++ static/js/main.js
@@ -3,24 +3,27 @@
 // Project Copilot Volunteer Client
 // Copyright 2016 Project Copilot
 
-(function () {
 
-  var provider = new firebase.auth.GoogleAuthProvider();
+var provider = new firebase.auth.GoogleAuthProvider();
 
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    var user = result.user;
-    // ...
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    var email = error.email;
-    var credential = error.credential;
-    // ...
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  var user = result.user;
+
+  var root = firebase.database().ref("/");
+  var volunteers = firebase.database().ref("volunteers");
+  var messages = firebase.database().ref("messages");
+
+
+  firebase.database().ref().child("messages").on("child_added", function (s) {
+    console.log(s.val());
   });
-
-  var db = firebase.database().ref("/");
-
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  var email = error.email;
+  var credential = error.credential;
+  // ...
 });
