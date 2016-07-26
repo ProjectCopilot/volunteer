300bd489f74dc79cf3275af47417809c53b011b8
gmittal <gautam@mittal.net>
2016-07-26 15:13:40 -0700

    uncommented

--- static/js/main.js
+++ static/js/main.js
@@ -23,35 +23,35 @@ var CURRENT_CASE = {};
 
 var provider = new firebase.auth.GoogleAuthProvider();
 
-// firebase.auth().signInWithPopup(provider).then(function(result) {
-//   // This gives you a Google Access Token. You can use it to access the Google API.
-//   var token = result.credential.accessToken;
-//   var user = result.user;
-//
-//   var root = firebase.database().ref("/");
-//   var volunteers = firebase.database().ref("volunteers");
-//   var messages = firebase.database().ref("messages");
-//
-//   firebase.database().ref().child("messages").on("child_added", function (s) {
-//     console.log(s.val());
-//   });
-//
-//   // new message input
-//   $("#mainInput").keydown(function (evt) {
-//       if (evt.keyCode == 13) {
-//         volunteers.child(user.email.replace(".", "")).push({"body": $("#mainInput").val()}, function() {
-//           $("#mainInput").val("");
-//         });
-//       }
-//   });
-//
-//
-//
-// }).catch(function(error) {
-//   // Handle Errors here.
-//   var errorCode = error.code;
-//   var errorMessage = error.message;
-//   var email = error.email;
-//   var credential = error.credential;
-//   // ...
-// });
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  var user = result.user;
+
+  var root = firebase.database().ref("/");
+  var volunteers = firebase.database().ref("volunteers");
+  var messages = firebase.database().ref("messages");
+
+  firebase.database().ref().child("messages").on("child_added", function (s) {
+    console.log(s.val());
+  });
+
+  // new message input
+  $("#mainInput").keydown(function (evt) {
+      if (evt.keyCode == 13) {
+        volunteers.child(user.email.replace(".", "")).push({"body": $("#mainInput").val()}, function() {
+          $("#mainInput").val("");
+        });
+      }
+  });
+
+
+
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  var email = error.email;
+  var credential = error.credential;
+  // ...
+});
